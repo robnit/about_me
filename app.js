20fc52fb033b27c6cd8fa89d144ea2a15b876dc4
Robin Miller <rmillerpdx@gmail.com>
2017-08-01 15:37:20 -0700

    added questions 1 through 3

--- app.js
+++ app.js
@@ -5,23 +5,70 @@ if (!confirm('By clicking OK, u agree to the terms and conditions of my Web site
 */
 var correctAnswers = 0;
 
-var q1 = prompt('please type a letter or a number');
+//Question 1
+var q1 = prompt('what is your name? (Please enter yes or no)');
 q1 = q1.toLowerCase();
 
-if (q1.length > 1){
-    alert ('too many characters, u failed');
+if (q1 === 'yes') {
+    correctAnswers++;
+    alert ('correct!');
+}
+else if (q1 === 'no'){
+    alert ('incorrect answer');
+}
+else {
+    alert ('incorrect answer');
 }
 
-else if (!RegExp(/[a-z0-9]/).test(q1)){
+//Question 2
+var q2 = prompt('where are you from? (Please enter yes or no');
+q2 = q2.toLowerCase();
+
+if (q2 === 'yes'){
+    alert ('incorrect answer');
+}
+else if (q2 === 'no'){
+    correctAnswers++;
+    alert ('correct!');
+}
+else {
+    alert ('incorrect answer');
+}
+
+//Question 3
+var q3 = prompt('how would you solve the housing crisis? (Please enter yes or no)');
+q3 = q3.toLowerCase();
+
+if (q3 === 'yes'){
+    alert ('incorrect answer');
+}
+else if (q3 === 'no'){
+    correctAnswers++;
+    alert ('correct!');
+}
+else {
+    alert ('incorrect answer');
+}
+
+
+// Bonus Question
+var bonusQ = prompt('please type a letter or a number');
+bonusQ = bonusQ.toLowerCase();
+
+if (bonusQ.length != 1){
+    alert ('only one character required');
+}
+
+else if (!RegExp(/[a-z0-9]/).test(bonusQ)){
     alert ('no forbidden characters allowed');
 }
 
-else if ((RegExp(/[a-z]/).test(q1))){
+else if ((RegExp(/[a-z]/).test(bonusQ))){
     correctAnswers++;
     alert ('good letter');
 }
 
-else if ((RegExp(/[0-9]/).test(q1))){
+else if ((RegExp(/[0-9]/).test(bonusQ))){
     correctAnswers++;
     alert ('that\'s a fine number');
 }
