0eae942f79843498f41631d916ad431ee5e974a3
Robin Miller <rmillerpdx@gmail.com>
2017-08-01 16:55:49 -0700

    add css markup and extra javascript

--- app.js
+++ app.js
@@ -10,14 +10,22 @@ var q1 = prompt('what is your name? (Please enter yes or no)');
 q1 = q1.toLowerCase();
 
 if (q1 === 'yes') {
-    correctAnswers++;
-    alert ('correct!');
+  correctAnswers++;
+  console.log('Correct answer guessed! User\'s score is ' + correctAnswers);
+  alert ('correct!');
 }
+
+else if (q1.startsWith("y")){
+  correctAnswers++;
+  console.log('Correct answer guessed! User\'s score is ' + correctAnswers);
+  alert ('it looks like you attempted to type \'yes\'');
+}
+
 else if (q1 === 'no'){
-    alert ('incorrect answer');
+  alert ('incorrect answer');
 }
 else {
-    alert ('incorrect answer');
+  alert ('invalid input');
 }
 
 //Question 2
@@ -25,14 +33,15 @@ var q2 = prompt('where are you from? (Please enter yes or no');
 q2 = q2.toLowerCase();
 
 if (q2 === 'yes'){
-    alert ('incorrect answer');
+  alert ('incorrect answer');
 }
 else if (q2 === 'no'){
-    correctAnswers++;
-    alert ('correct!');
+  correctAnswers++;
+  console.log('Correct answer guessed! User\'s score is ' + correctAnswers);
+  alert ('correct!');
 }
 else {
-    alert ('incorrect answer');
+  alert ('invalid input');
 }
 
 //Question 3
@@ -40,35 +49,69 @@ var q3 = prompt('how would you solve the housing crisis? (Please enter yes or no
 q3 = q3.toLowerCase();
 
 if (q3 === 'yes'){
-    alert ('incorrect answer');
+  alert ('incorrect answer');
 }
 else if (q3 === 'no'){
-    correctAnswers++;
-    alert ('correct!');
+  correctAnswers++;
+  console.log('Correct answer guessed! User\'s score is ' + correctAnswers);
+  alert ('correct!');
 }
 else {
-    alert ('incorrect answer');
+  alert ('invalid input');
 }
 
+//Question 4
+var q4 = prompt('Do I enjoy rock climbing, burritos, and chiptune music? (Please enter yes or no)');
+q4 = q4.toLowerCase();
+
+if (q4 === 'yes'){
+  correctAnswers++;
+  console.log('Correct answer guessed! User\'s score is ' + correctAnswers);
+  alert ('correct!');
+}
+else if (q4 === 'no'){
+  alert ('incorrect answer');
+}
+else {
+  alert ('invalid input');
+}
+
+//Question 5
+var q5 = prompt('How are you? (Please enter yes or no)');
+q4 = q5.toLowerCase();
+
+if (q5 === 'yes'){
+  correctAnswers++;
+  console.log('Correct answer guessed! User\'s score is ' + correctAnswers);
+  alert ('correct!');
+}
+else if (q5 === 'no'){
+  alert ('incorrect answer');
+}
+else {
+  alert ('invalid input');
+}
 
 // Bonus Question
-var bonusQ = prompt('please type a letter or a number');
+var bonusQ = prompt('***BONUS QUESTION***\n please type a letter or a number');
 bonusQ = bonusQ.toLowerCase();
 
 if (bonusQ.length != 1){
-    alert ('only one character required');
+  alert ('only one character required');
 }
 
 else if (!RegExp(/[a-z0-9]/).test(bonusQ)){
-    alert ('no forbidden characters allowed');
+  alert ('no forbidden characters allowed');
 }
 
 else if ((RegExp(/[a-z]/).test(bonusQ))){
-    correctAnswers++;
-    alert ('good letter');
+  correctAnswers++;
+  console.log('Correct answer guessed! User\'s score is ' + correctAnswers);
+  alert ('good letter');
 }
 
 else if ((RegExp(/[0-9]/).test(bonusQ))){
-    correctAnswers++;
-    alert ('that\'s a fine number');
+  correctAnswers++;
+  console.log('Correct answer guessed! User\'s score is ' + correctAnswers);
+  alert ('that\'s a fine number');
 }
