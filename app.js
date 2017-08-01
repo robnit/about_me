27a6731c9e91dfa0ce8d8639250df6888b763c65
Robin Miller <rmillerpdx@gmail.com>
2017-08-01 15:11:55 -0700

    added regex-based bonus question

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
-if (!confirm("By clicking OK, u agree to the terms and conditions of my Web site")){
-    alert("I'll let you in anyways");
+/*
+if (!confirm('By clicking OK, u agree to the terms and conditions of my Web site')){
+    alert('I\'ll let you in anyways');
+}
+*/
+var correctAnswers = 0;
+
+var q1 = prompt('please type a letter or a number');
+q1 = q1.toLowerCase();
+
+if (q1.length > 1){
+    alert ('too many characters, u failed');
+}
+
+else if (!RegExp(/[a-z0-9]/).test(q1)){
+    alert ('no forbidden characters allowed');
+}
+
+else if ((RegExp(/[a-z]/).test(q1))){
+    correctAnswers++;
+    alert ('good letter');
+}
+
+else if ((RegExp(/[0-9]/).test(q1))){
+    correctAnswers++;
+    alert ('that\'s a fine number');
 }
