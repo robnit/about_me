1dcaacd4bc3b874ff6255d80234d345eaa3263ae
Robin Miller <rmillerpdx@gmail.com>
2017-08-02 13:18:55 -0700

    simplify q var declarations

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ if (!confirm('By clicking OK, u agree to the terms and conditions of my Web site
 var correctAnswers = 0;
 
 //Question 1
-var q1 = prompt('what is your name? (Please enter yes or no)');
-q1 = q1.toLowerCase();
+var q1 = prompt('what is your name? (Please enter yes or no)').toLowerCase();
 
 if (q1 === 'yes') {
   correctAnswers++;
@@ -15,27 +14,26 @@ if (q1 === 'yes') {
   alert ('correct!');
 }
 
-else if (q1.startsWith("y")){
+else if (q1.startsWith('y')){
   correctAnswers++;
   console.log('Correct answer guessed! User\'s score is ' + correctAnswers);
   alert ('it looks like you attempted to type \'yes\'');
 }
 
 else if (q1 === 'no'){
-  console.log("User has entered incorrect input");
+  console.log('User has entered incorrect input');
   alert ('incorrect answer');
 }
 else {
-  console.log("User has entered incorrect input");
+  console.log('User has entered incorrect input');
   alert ('invalid input');
 }
 
 //Question 2
-var q2 = prompt('where are you from? (Please enter yes or no');
-q2 = q2.toLowerCase();
+var q2 = prompt('where are you from? (Please enter yes or no').toLowerCase();
 
 if (q2 === 'yes'){
-  console.log("User has entered incorrect input");
+  console.log('User has entered incorrect input');
   alert ('incorrect answer');
 }
 else if (q2 === 'no'){
@@ -44,16 +42,15 @@ else if (q2 === 'no'){
   alert ('correct!');
 }
 else {
-  console.log("User has entered incorrect input");
+  console.log('User has entered incorrect input');
   alert ('invalid input');
 }
 
 //Question 3
-var q3 = prompt('how would you solve the housing crisis? (Please enter yes or no)');
-q3 = q3.toLowerCase();
+var q3 = prompt('how would you solve the housing crisis? (Please enter yes or no)').toLowerCase();
 
 if (q3 === 'yes'){
-  console.log("User has entered incorrect input");
+  console.log('User has entered incorrect input');
   alert ('incorrect answer');
 }
 else if (q3 === 'no'){
@@ -62,13 +59,12 @@ else if (q3 === 'no'){
   alert ('correct!');
 }
 else {
-  console.log("User has entered incorrect input");
+  console.log('User has entered incorrect input');
   alert ('invalid input');
 }
 
 //Question 4
-var q4 = prompt('Do I enjoy rock climbing, burritos, and chiptune music? (Please enter yes or no)');
-q4 = q4.toLowerCase();
+var q4 = prompt('Do I enjoy rock climbing, burritos, and chiptune music? (Please enter yes or no)').toLowerCase();
 
 if (q4 === 'yes'){
   correctAnswers++;
@@ -76,17 +72,16 @@ if (q4 === 'yes'){
   alert ('correct!');
 }
 else if (q4 === 'no'){
-  console.log("User has entered incorrect input");
+  console.log('User has entered incorrect input');
   alert ('incorrect answer');
 }
 else {
-  console.log("User has entered incorrect input");
+  console.log('User has entered incorrect input');
   alert ('invalid input');
 }
 
 //Question 5
-var q5 = prompt('How are you? (Please enter yes or no)');
-q4 = q5.toLowerCase();
+var q5 = prompt('How are you? (Please enter yes or no)').toLowerCase();
 
 if (q5 === 'yes'){
   correctAnswers++;
@@ -94,25 +89,24 @@ if (q5 === 'yes'){
   alert ('correct!');
 }
 else if (q5 === 'no'){
-  console.log("User has entered incorrect input");
+  console.log('User has entered incorrect input');
   alert ('incorrect answer');
 }
 else {
-  console.log("User has entered incorrect input");
+  console.log('User has entered incorrect input');
   alert ('invalid input');
 }
 
 // Bonus Question
-var bonusQ = prompt('***BONUS QUESTION***\n please type a letter or a number');
-bonusQ = bonusQ.toLowerCase();
+var bonusQ = prompt('***BONUS QUESTION***\n please type a letter or a number').toLowerCase();
 
 if (bonusQ.length != 1){
-  console.log("User has entered incorrect input");
+  console.log('User has entered incorrect input');
   alert ('only one character required');
 }
 
 else if (!RegExp(/[a-z0-9]/).test(bonusQ)){
-  console.log("User has entered incorrect input");
+  console.log('User has entered incorrect input');
   alert ('no forbidden characters allowed');
 }
 
