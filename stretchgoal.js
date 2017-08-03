c18478582cb8a16c1b05a057506bc0f4c6ef8769
Robin Miller <rmillerpdx@gmail.com>
2017-08-03 11:27:50 -0700

    add functional array version, case sensitive

--- stretchgoal.js
+++ stretchgoal.js
@@ -1,34 +1,41 @@
 //Only basic yes/no inputs expected (for now)
 var myArray = [
-            ['how old are you? (Please enter yes or no)', 'yes', 0],
-            ['where are you from? (Please enter yes or no', 'no', 0],
-            ['how would you solve the housing crisis? (Please enter yes or no)', 'no', 0],
-            ['Do I enjoy rock climbing, burritos, and chiptune music? (Please enter yes or no)', 'yes', 0],
-            ['How are you? (Please enter yes or no)', 'yes', 0]
-            ];
+  ['how old are you? (Please enter yes or no)', 'yes', 0],
+  ['where are you from? (Please enter yes or no', 'no', 0],
+  ['how would you solve the housing crisis? (Please enter yes or no)', 'no', 0],
+  ['Do I enjoy rock climbing, burritos, and chiptune music? (Please enter yes or no)', 'yes', 0],
+  ['How are you? (Please enter yes or no)', 'yes', 0]
+];
 
 for (i = 0; i < myArray.length; i++){
-    
+  if (prompt(myArray[i][0]) == myArray[i][1]){ //why cant i add tolowercase to this
+    myArray[i][2] = 1;
+    alert ('that\s right!');
+  }
+  else {
+    alert ('NO');
+  }
 }
+
 
 
 //VVVVVOriginal code VVVVVV
-//Question 1
-var q1 = prompt('how old are you? (Please enter yes or no)').toLowerCase();
+// //Question 1
+// var q1 = prompt('how old are you? (Please enter yes or no)').toLowerCase();
 
-if (q1 === 'yes') {
-  correctAnswers++;
-  console.log('Correct answer guessed! User\'s score is ' + correctAnswers);
-  alert ('correct!');
-}
-else if (q1 === 'no'){
-  console.log('User has entered incorrect input');
-  alert ('incorrect answer');
-}
-else {
-  console.log('User has entered incorrect input');
-  alert ('invalid input');
-}
+// if (q1 === 'yes') {
+//   correctAnswers++;
+//   console.log('Correct answer guessed! User\'s score is ' + correctAnswers);
+//   alert ('correct!');
+// }
+// else if (q1 === 'no'){
+//   console.log('User has entered incorrect input');
+//   alert ('incorrect answer');
+// }
+// else {
+//   console.log('User has entered incorrect input');
+//   alert ('invalid input');
+// }
 
 // //Question 2
 // var q2 = prompt('where are you from? (Please enter yes or no').toLowerCase();
